606d4e42e199c2fe92b3c978ca63a3c576597b0d
Aidan Amato <aidanamato@comcast.net>
2021-11-03 22:24:45 -0400

    add useState to track current category and useEffect to update document.title

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,6 +6,7 @@ function About() {
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{width: "100%"}} alt="cover"/>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci </p>
     </section>
   );
 }
