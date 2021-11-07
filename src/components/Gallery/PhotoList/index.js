8462cf3d6507cd445635b20f3433ad8494b9141c
Aidan Amato <aidanamato@comcast.net>
2021-11-07 17:16:13 -0500

    add conditional rendering of modal when an image is clicked

--- src/components/Gallery/PhotoList/index.js
+++ src/components/Gallery/PhotoList/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Modal from "./Modal";
 
 function PhotoList(props) {
   const { category } = props;
@@ -100,14 +101,24 @@ function PhotoList(props) {
 
   const currentPhotos = photos.filter(photo => photo.category === category);
   
+  const [isModalOpen, setIsModalOpen] =useState(false);
+
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({...image, index: i})
+    setIsModalOpen(true);
+  }
+  
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
